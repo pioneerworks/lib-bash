c0e0f134863e1761191ac518f6fe6f42b047bf11
Konstantin Gredeskoul <kigster@gmail.com>
2018-02-28 13:45:16 -0800

    Caching last rebased at for performance

--- lib/Loader.bash
+++ lib/Loader.bash
@@ -4,7 +4,7 @@ export True=1
 export False=0
 
 export LibBash__SearchTarget="Loader.bash"
-export LibBash__Loader=$(find -L . -maxdepth 3 -type f -name "${LibBash__SearchTarget}" -print 2>/dev/null)
+export LibBash__Loader=$(find -L . -maxdepth 3 -type f -name "${LibBash__SearchTarget}" -print 2>/dev/null | tail -1)
 
 if [[ -z ${LibBash__Loader} ]]; then
   printf "${bldred}ERROR: ${clr}Can not find ${bldylw}${LibBash__SearchTarget}${clr} file, aborting."
@@ -18,16 +18,13 @@ lib::bash-source() {
 
   [[ -n ${DEBUG} ]] && printf "sourcing folder ${bldylw}$folder${clr}...\n" >&2
   # Let's list all lib files
-  declare -a files=($(ls -1 ${folder}/[a-z]*.sh))
+  declare -a files=($(ls -1 ${folder}/*.sh))
 
   for bash_file in ${files[@]}; do
     [[ -n ${DEBUG} ]] && printf "sourcing ${txtgrn}$bash_file${clr}...\n" >&2
-    set +e
-    [[ $(basename ${bash_file}) == "Loader.bash" ]] || source ${bash_file}
+    source ${bash_file}
   done
 }
 
-[[ -f ${LibBash__LibDir}/settings.sh ]] && source ${LibBash__LibDir}/settings.sh
-
-lib::bash-source ${LibBash__LibDir}
+[[ -n ${LibBash__LibDir} ]] && lib::bash-source "${LibBash__LibDir}"
 
