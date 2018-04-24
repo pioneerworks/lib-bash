5fc4c7d6e18877de6e41ed2a07b16440f6a6c0d1
Konstantin Gredeskoul <kigster@gmail.com>
2018-04-23 17:58:08 -0700

    Adding Ruby Gem helpers for installing gems
     * Also adding help/comments/copyright.

--- lib/gem.sh
+++ lib/gem.sh
@@ -1,32 +1,12 @@
 #!/usr/bin/env bash
-
-lib::ruby::gemfile-lock-version() {
-  local gem=${1}
-
-  if [[ ! -f Gemfile.lock ]]; then
-    error "Can not find Gemfile.lock"
-    return 1
-  fi
-
-  egrep " ${gem} \([0-9]" Gemfile.lock | sed -e 's/[\(\)]//g' | awk '{print $2}'
-}
-
-lib::ruby::bundler-version() {
-  if [[ ! -f Gemfile.lock ]]; then
-    error "Can not find Gemfile.lock"
-    return 1
-  fi
-  tail -1 Gemfile.lock | sed 's/ //g'
-}
-
-
-#———————————————————————————————————————————————————————————————————————————————
+#g
 # GEM dependencies
-#———————————————————————————————————————————————————————————————————————————————
-# This extracts a gem version from Gemfile.lock. If not found,
+#
+# This extracts a lib::gem::version from Gemfile.lock. If not found,
 # default (argument) is used. This helps prevent version mismatch
 # between the very few gem dependencies of the zeus subsystem, and Rails.
-gem.version() {
+
+lib::gem::version() {
   local gem=$1
   local default=$2
   local version
@@ -34,7 +14,7 @@ gem.version() {
   if [[ -f './Gemfile.lock' ]]; then
     version=$(egrep "${gem} \(\d+\.\d+\.\d+\(\.\d+\)?\)" Gemfile.lock | awk '{print $2}' | sed 's/[()]//g')
   else
-    gem.list.load
+    lib::gem::load-list
     version=$(gem list | egrep "${gem}" | awk '{print $2}' | sed -E 's/[()]//g')
   fi
 
@@ -42,34 +22,36 @@ gem.version() {
   printf "%s" ${version}
 }
 
-gem.list.load() {
+lib::gem::load-list() {
   if [[ ! -s "${GEM_LIST_FILE}" || -z $(find "${GEM_LIST_FILE}" -mmin -30) ]]; then
     gem list > "${GEM_LIST_FILE}"
   fi
 }
 
-gem.is_installed() {
+lib::gem::is-installed() {
   local gem=$1
   local version=$2
 
-  gem.list.load
+  lib::gem::load-list
 
   if [[ -z ${version} ]]; then
     egrep "${gem} \(" "${GEM_LIST_FILE}"
   else
-    egrep "${gem} \(" "${GEM_LIST_FILE}" | grep ${version}
+    egrep "${gem} \(" "${GEM_LIST_FILE}" | grep "${version}"
   fi
 }
 
 # Install the gem, but use the version argument as a default. Final version
-# is determined from Gemfile.lock using the +gem.version+ above.
-gem.install() {
+# is determined from Gemfile.lock using the +lib::gem::version+ above.
+lib::gem::install() {
   local gem_name=$1
   local gem_version=$2
-  gem_version=$(gem.version ${gem_name} ${gem_version})
-  if [[ -z $(gem.is_installed ${gem_name} ${gem_version}) ]]; then
+
+  gem_version=$(lib::gem::version ${gem_name} ${gem_version})
+
+  if [[ -z $(lib::gem::is-installed ${gem_name} ${gem_version}) ]]; then
     printf "(${txtylw}installing${clr}) ... ${bldred}"
-    gem install ${gem_name} --version ${gem_version} 1>/dev/null
+    lib::gem::install ${gem_name} --version ${gem_version} 1>/dev/null
     result=$?
     printf ${clr}
     [[ $result == 0 ]] && printf "${bldgrn} ✔ ${clr}\n"
