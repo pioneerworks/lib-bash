9f09952707321d7293e6a7787b3274907406b96d
Konstantin Gredeskoul <kigster@gmail.com>
2018-04-23 18:57:07 -0700

    This commit further fixes gem version management.
    We add a few gem version functions, in particular
    
     * lib::gem::install
     * lib::gem::ensure-gem-version
     * lib::gem::version
    
    And more.

--- lib/gem.sh
+++ lib/gem.sh
@@ -5,7 +5,9 @@
 # This extracts a lib::gem::version from Gemfile.lock. If not found,
 # default (argument) is used. This helps prevent version mismatch
 # between the very few gem dependencies of the zeus subsystem, and Rails.
+
 export LibGem__GemListCache=/tmp/gem_list.txt
+export LibGem__GemInstallFlags="--no-ri --no-rdoc --force --quiet"
 
 lib::gem::version() {
   local gem=$1
@@ -16,16 +18,30 @@ lib::gem::version() {
     version=$(egrep "${gem} \(\d+\.\d+\.\d+\(\.\d+\)?\)" Gemfile.lock | awk '{print $2}' | sed 's/[()]//g')
   else
     lib::gem::load-list
-    version=$(gem list | egrep "${gem}" | awk '{print $2}' | sed -E 's/[()]//g')
+    version=$(gem list | egrep "${gem}" | awk '{print $2}' | sed -E 's/[(),]//g')
   fi
 
   version=${version:-${default}} # fallback to the default if not found
   printf "%s" ${version}
 }
 
+# this ensures the cache is only at most 30 minutes old
 lib::gem::load-list() {
   if [[ ! -s "${LibGem__GemListCache}" || -z $(find "${LibGem__GemListCache}" -mmin -30) ]]; then
-    gem list > "${LibGem__GemListCache}"
+    run "gem list > ${LibGem__GemListCache}"
+  fi
+}
+
+
+lib::gem::ensure-gem-version() {
+  local gem=$1
+  local gem_version=$2
+
+  if [[ -z $(gem list | grep "${gem} (${gem_version})") ]]; then
+    run "gem uninstall --all --force -x -I ${gem}"
+    run "gem install ${gem} --version ${gem_version} ${LibGem__GemInstallFlags}"
+  else
+    info "gem ${gem} version ${gem_version} is already installed."
   fi
 }
 
@@ -47,22 +63,25 @@ lib::gem::is-installed() {
 lib::gem::install() {
   local gem_name=$1
   local gem_version=$2
+  local gem_version_flags=
+  local gem_version_name=
 
   gem_version=$(lib::gem::version ${gem_name} ${gem_version})
 
-  if [[ -z $(lib::gem::is-installed ${gem_name} ${gem_version}) ]]; then
-    printf "    (${txtylw}installing${clr}) ... ${bldred}"
-    lib::gem::install ${gem_name} --version ${gem_version} 1>/dev/null
-    result=$?
-    printf ${clr}
-    [[ ${result} == 0 ]] && printf "${bldgrn} ✔ ${clr}\n"
-    [[ ${result} != 0 ]] && printf "${bldred} FAILED ${clr} with code ${result}\n"
-    [[ ${result} != 0 ]] && {
-      [[ ${_is_sourced} == "no" ]] && exit 1
-      [[ ${_is_sourced} == "no" ]] || return
-    }
-    rbenv rehash >/dev/null
+  if [[ -z ${gem_version} ]]; then
+    gem_version_name=latest
+    gem_version_flags=
   else
-    printf "${bldgrn} ✔ ${clr} (already installed)\n"
+    gem_version_name="${gem_version}"
+    gem_version_flags="--version ${gem_version}"
+  fi
+
+  if [[ -z $(lib::gem::is-installed ${gem_name} ${gem_version}) ]]; then
+    info "installing ${bldylw}${gem_name} ${bldgrn}(${gem_version_name})${txtblu}..."
+    run "gem install ${gem_name} ${gem_version_flags} ${LibGem__GemInstallFlags}"
+    rbenv rehash >/dev/null
+    return ${LibRun__LastExitCode}
+  else
+    info: "gem ${bldylw}${gem_name} (${bldgrn}${gem_version_name}${bldylw})${txtblu} is already installed"
   fi
 }
--- lib/ruby.sh
+++ lib/ruby.sh
@@ -19,67 +19,6 @@ lib::ruby::bundler-version() {
   tail -1 Gemfile.lock | sed 's/ //g'
 }
 
-
-#———————————————————————————————————————————————————————————————————————————————
-# GEM dependencies
-#———————————————————————————————————————————————————————————————————————————————
-# This extracts a gem version from Gemfile.lock. If not found,
-# default (argument) is used. This helps prevent version mismatch
-# between the very few gem dependencies of the zeus subsystem, and Rails.
-gem.version() {
-  local gem=$1
-  local default=$2
-  local version
-
-  if [[ -f './Gemfile.lock' ]]; then
-    version=$(egrep "${gem} \(\d+\.\d+\.\d+\(\.\d+\)?\)" Gemfile.lock | awk '{print $2}' | sed 's/[()]//g')
-  else
-    gem.list.load
-    version=$(gem list | egrep "${gem}" | awk '{print $2}' | sed -E 's/[()]//g')
-  fi
-
-  version=${version:-${default}} # fallback to the default if not found
-  printf "%s" ${version}
-}
-
-gem.list.load() {
-  if [[ ! -s "${GEM_LIST_FILE}" || -z $(find "${GEM_LIST_FILE}" -mmin -30) ]]; then
-    gem list > "${GEM_LIST_FILE}"
-  fi
-}
-
-gem.is_installed() {
-  local gem=$1
-  local version=$2
-
-  gem.list.load
-
-  if [[ -z ${version} ]]; then
-    egrep "${gem} \(" "${GEM_LIST_FILE}"
-  else
-    egrep "${gem} \(" "${GEM_LIST_FILE}" | grep ${version}
-  fi
-}
-
-# Install the gem, but use the version argument as a default. Final version
-# is determined from Gemfile.lock using the +gem.version+ above.
-gem.install() {
-  local gem_name=$1
-  local gem_version=$2
-  gem_version=$(gem.version ${gem_name} ${gem_version})
-  if [[ -z $(gem.is_installed ${gem_name} ${gem_version}) ]]; then
-    printf "(${txtylw}installing${clr}) ... ${bldred}"
-    gem install ${gem_name} --version ${gem_version} 1>/dev/null
-    result=$?
-    printf ${clr}
-    [[ $result == 0 ]] && printf "${bldgrn} ✔ ${clr}\n"
-    [[ $result != 0 ]] && printf "${bldred} FAILED ${clr} with code ${result}\n"
-    [[ $result != 0 ]] && {
-      [[ ${_is_sourced} == "no" ]] && exit 1
-      [[ ${_is_sourced} == "no" ]] || return
-    }
-    rbenv rehash >/dev/null
-  else
-    printf "${bldgrn} ✔ ${clr} (already installed)\n"
-  fi
+lib::ruby::version() {
+  ruby --version
 }
