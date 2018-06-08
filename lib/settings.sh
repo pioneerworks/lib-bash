f3c42d338c94c48f83293c86f5388989e3098289
Konstantin Gredeskoul <kigster@gmail.com>
2018-06-08 01:24:45 -0700

    Removing Sym and most settings

--- lib/settings.sh
+++ lib/settings.sh
@@ -3,47 +3,5 @@
 export True="1"
 export False="0"
 
-export HomebaseNodeVersion="9.4.0"
-export HomebaseNvmVersion="0.33.2"
-
-export HomebasePostgresVersion="9.4"
-export HomebasePostgresHostname="localhost"
-export HomebasePostgresUsername="postgres"
-
-export HomebaseCurrentOS=$(uname -s)
-
-if [[ -f ".ruby-version" ]]; then
-  export HomebaseRubyVersion=$(cat .ruby-version)
-else
-  export HomebaseRubyVersion="2.2.7"
-fi
-
-declare -a HomebaseBrewPackages=(
-    autoconf
-    autogen
-    automake
-    awscli
-    bash
-    bash-completion
-    coreutils
-    chromedriver
-    curl
-    direnv
-    go
-    htop
-    hub
-    imagemagick
-    jq
-    memcached
-    openssl
-    phantomjs
-    rbenv
-    redis
-    ruby-build
-    the_silver_searcher
-    wget
-    yarn
-  )
-
-export HomebaseBrewPackages
-export HomebaseDefaultBackupDir='tmp/pgdump'
+export LibBashInstallerURL="https://raw.githubusercontent.com/pioneerworks/lib-bash/master/bin/bootstrap"
+export LibBashInstallerScriptUrl="https://raw.githubusercontent.com/pioneerworks/lib-bash/master/bin/install?q=$RANDOM"
